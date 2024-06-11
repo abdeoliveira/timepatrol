0d68ff16c6a3d0f475e7db14246472d60f8ecad7
Alan B. de Oliveira <jepridaz@gmail.com>
2024-06-11 12:58:22 -0300

    check

--- arch-install.sh
+++ arch-install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 PATH_LIBALPM=/usr/share/libalpm
+CONFIG_DIR=/etc/timepatrol
+
 chmod +x timepatrol-pre
 chmod +x timepatrol
+
 cp 05-timepatrol-pre.hook $PATH_LIBALPM/hooks
 cp timepatrol-pre $PATH_LIBALPM/scripts
 cp timepatrol /usr/local/bin/
+
+mkdir -p $CONFIG_DIR
 cp config /etc/timepatrol/
+
