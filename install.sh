90b23b91cdfbf8dbb69b6cd3adfb338f0f5ad016
Alan B. de Oliveira <jepridaz@gmail.com>
2024-06-11 09:02:33 -0300

    included pacman hooks

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PATH_LIBALPM=/usr/share/libalpm
+chmod +x timepatrol-pre
+chmod +x timepatrol
+cp 05-timepatrol-pre.hook $PATH_LIBALPM/hooks
+cp timepatrol-pre $PATH_LIBALPM/scripts
+cp timepatrol /usr/local/bin/
