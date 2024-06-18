7c1e94665dcc8d000fb0e01f0461aac6fd2f1ba6
Alan B. de Oliveira <jepridaz@gmail.com>
2024-06-18 09:21:46 -0300

    fix

--- arch-install.sh
+++ arch-install.sh
@@ -3,16 +3,16 @@ PATH_LIBALPM=/usr/share/libalpm
 CONFIG_DIR=/etc/timepatrol
 
 ## INSTALLS THE SCRIPT AND CONFIG FILE 
+chmod +x timepatrol
 cp timepatrol /usr/local/bin/
 mkdir -p $CONFIG_DIR
 cp config $CONFIG_DIR
 
-## COMMENT THE LINES BELOW IF YOU DON'T WANT THE HOOKS.
-chmod +x timepatrol-pre
-chmod +x timepatrol
-cp 00-timepatrol-pre.hook $PATH_LIBALPM/hooks/
+## COMMENT IF YOU DON'T WANT THE PACMAN HOOKS.
+chmod +x timepatrol-pacman
+rm $PATH_LIBALPM/scripts/timepatrol-*
+cp timepatrol-pacman $PATH_LIBALPM/scripts/
+rm $PATH_LIBALPM/hooks/*-timepatrol-*.hook
+cp 05-timepatrol-pre.hook $PATH_LIBALPM/hooks/
 cp zz-timepatrol-post.hook $PATH_LIBALPM/hooks/
-cp timepatrol-pre $PATH_LIBALPM/scripts/
-cp timepatrol-post $PATH_LIBALPM/scripts/
-
 
